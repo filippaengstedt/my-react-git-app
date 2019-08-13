2ccd1b9884984a4a7f888cf071c5e6ac7e523971
Filippa Engstedt <filippa.engstedt@ffcg.se>
2019-08-13 15:15:38 +0200

    new files added

---
+++ src/components/Comp1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Comp1 extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Comp1
---
+++ src/components/Comp2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Comp2 extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Comp2
